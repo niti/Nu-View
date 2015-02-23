9ea016e946b07f153472ee518431c3827b3a832c
Allison <amm4yv@virginia.edu>
2015-02-22 21:35:43 -0500

    Added missing files

---
+++ BarApp/BarLibrary.swift
@@ -0,0 +1,56 @@
+//
+//  BarLibrary.swift
+//  BarApp
+//
+//  Created by Allison Moyer on 2/17/15.
+//  Copyright (c) 2015 Allison Moyer. All rights reserved.
+//
+
+import Foundation
+
+struct BarLibrary {
+    let library = [
+        
+        [
+            "title": "Biltmore",
+            "icon": "biltmore.png",
+        ],
+        
+        [
+            "title": "Boylan",
+            "icon": "boylan.png",
+        ],
+        
+        [
+            "title": "Coupes",
+            "icon": "coupes.png",
+        ],
+        
+        [
+            "title": "Crozet",
+            "icon": "crozet.png",
+        ],
+        
+        [
+            "title": "Michael's Bistro",
+            "icon": "michaels.png",
+        ],
+        
+        [
+            "title": "St. Maarten",
+            "icon": "stmaarten.png",
+        ],
+        
+        [
+            "title": "Trinity",
+            "icon": "trinity.png",
+        ],
+        
+        [
+            "title": "Virginian",
+            "icon": "virginian.png",
+        ]
+
+    ]
+
+}
